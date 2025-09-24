6e7173feed56148bc9b60735ad72b04cd8b38ed6
RSP-007 <217321612+RSP-007@users.noreply.github.com>
2025-09-24 19:09:10 +0530

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-//var generateName = require('sillyname'); ESM method "type":"commonjs"
-
-import generateName from "sillyName"; // CJS METHOD -change to "type":"module" for using import csj method
-var sillyName = generateName();
-console.log(`My name is ${sillyName}.`);
